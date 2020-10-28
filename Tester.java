1549be9f98170531bd54bbfec8d718db06cdd080
J3rry-L <jliang21@stuy.edu>
2020-10-28 16:38:01 -0400

    Added more tests and introduced Triangle

--- Tester.java
+++ Tester.java
@@ -2,7 +2,9 @@ public class Tester {
   public static void main (String[] args){
     Point A = new Point(1.1, 2.3);
     Point B = new Point(7.1, 10.3);
-    Point C = new Point(A);
+    Point C = new Point(7.1,2.3);
+    Point D = new Point(A);
+    Triangle rightTri1 = new Triangle(A,B,C);
     System.out.println(A.getX());
     System.out.println(A.getY());
     System.out.println(B.getX());
@@ -11,9 +13,12 @@ public class Tester {
     System.out.println(B.distanceTo(A));
     System.out.println(A.distanceTo(A));
     System.out.println(B.distanceTo(B));
-    System.out.println(C.equals(A));
-    System.out.println(A.equals(C));
-    System.out.println(C.distanceTo(A));
-    System.out.println(A.distanceTo(C));
+    System.out.println(D.equals(A));
+    System.out.println(A.equals(D));
+    System.out.println(D.distanceTo(A));
+    System.out.println(A.distanceTo(D));
+    System.out.println(A.equals(B));
+    System.out.println(B.equals(A));
+
   }
 }
